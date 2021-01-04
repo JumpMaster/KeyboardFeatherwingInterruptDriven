09a865891f5c16d568915be7fb687f40e65923d4
JumpMaster <kevin@cooper.uk>
2021-01-04 21:13:41 +0000

    Add interrupt resets

--- src/KeyboardFeatherwingInterruptDriven.cpp
+++ src/KeyboardFeatherwingInterruptDriven.cpp
@@ -56,44 +56,61 @@ void setup() {
 
   tft.print("Hello FeatherWing!\n");
 
-  ts.writeRegister8(STMPE_GPIO_ALT_FUNCT, _BV(2));  // Set pin 2 to GPIO
-  ts.writeRegister8(0x16, _BV(2)); // Pin 2 falling edge detection enable
+  ts.writeRegister8(STMPE_GPIO_ALT_FUNCT, _BV(2));  // Set pin 2 to alternate function
+  ts.writeRegister8(0x15, _BV(2)); // Pin 2 rising edge detection enable
 
-  ts.writeRegister8(STMPE_INT_EN, STMPE_INT_EN_TOUCHDET | STMPE_INT_EN_GPIO); // Interrupts for screen and GPIO
-  // ts.writeRegister8(STMPE_INT_EN, STMPE_INT_EN_GPIO); // Interrupts for GPIO only
+//   ts.writeRegister8(STMPE_INT_EN, STMPE_INT_EN_TOUCHDET | STMPE_INT_EN_GPIO); // Interrupts for screen and GPIO
+  ts.writeRegister8(STMPE_INT_EN, STMPE_INT_EN_GPIO); // Interrupts for GPIO only
   ts.writeRegister8(0x0C, 1 << 2); // Pin 2 GPIO interrupt enable
   
   pinMode(interruptPin, INPUT_PULLDOWN);
-  attachInterrupt(interruptPin, KeyIsr, RISING);
+  attachInterrupt(interruptPin, KeyIsr, FALLING);
 }
-
 
 void loop() {
 
   if (dataReady) {
-    tft.print("Interrupt received!\n");
+    pixels.setPixelColor(0, pixels.Color(0, 0, 255));
+    pixels.show();
 
-    // reset all interrupts
+    // Reset interrupt status register
     ts.writeRegister8(STMPE_INT_STA, 0xFF);
 
-    // Clear the GPIO interrupt status register
-    ts.writeRegister8(0x0D, 0xFF);  
+    // Reset the GPIO interrupt status register
+    ts.writeRegister8(0x0D, 0xFF);
+
+    // Reset GPIO edge detect status
+    ts.writeRegister8(0x14, 0xFF);
+
+    const BBQ10Keyboard::KeyEvent key = keyboard.keyEvent();
+
+    if (key.state == BBQ10Keyboard::StateLongPress) {
+    } else if (key.state == BBQ10Keyboard::StateRelease) {
+    } else if (key.state == BBQ10Keyboard::StatePress) {
+      // Print keys to screen
+      tft.print(key.key);
+    }
+
+    pixels.clear();
+    pixels.show();
 
     keyboard.clearInterruptStatus();
     dataReady = false;
   }
 
-  // Print keys to screen
-  if (keyboard.keyCount()) {
-    const BBQ10Keyboard::KeyEvent key = keyboard.keyEvent();
-    if (key.state == BBQ10Keyboard::StateRelease) {
-      pixels.setPixelColor(0, pixels.Color(0, 255, 0));
-      pixels.show();
-    
-      tft.print(key.key);
-      
-      pixels.clear();
-      pixels.show(); 
-    }
+  // Interrupts sometimes get stuck. If so clear them.
+  int reg = ts.readRegister8(0x0B);
+  if (reg) {
+      ts.writeRegister8(0x0B, 0xFF);
+  }
+
+  reg = ts.readRegister8(0x14);
+  if (reg) {
+      ts.writeRegister8(0x14, 0xFF);
+  }
+
+  reg = ts.readRegister8(0x0D);
+  if (reg) {
+      ts.writeRegister8(0x0d, 0xFF);
   }
 }
